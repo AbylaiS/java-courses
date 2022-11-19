ddc88b13bc669eee2efee4d4ec39161efdd414a9
AbyaliS <iridium.channel@gmail.com>
2022-11-19 14:00:25 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a = 10;
+        int b = 12;
+        int c = a * b;
+        System.out.println(c);
     }
 }
